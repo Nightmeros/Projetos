88274c8db353d627aac13db116f7869e505d066c
Nightmeros <pereiraguilherme970@gmail.com>
2025-12-05 14:32:11 -0300

    desenvolvimento da tela de login

--- GeratTextoComIA/resources/views/index.blade.php
+++ GeratTextoComIA/resources/views/index.blade.php
@@ -1,17 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
+
 <body>
-    <h1>Novo teste</h1>
-    <form action="" method="post">
-        <label for="email">Email</label>
-        <input type="email" name="email" id="email">
-        <label for="senha">Senha</label>
-        <input type="password" name="senha" id="senha">
-    </form>
+    <div class="centralizar">
+        <div id="logindivs">
+            <h1>Novo teste</h1>
+            <form action="" method="post">
+                <label for="email">Email: </label>
+                <input type="email" name="email" id="email">
+                <br><br>
+                <label for="senha">Senha: </label>
+                <input type="password" name="senha" id="senha">
+                <a href="#">Esqueci a senha</a>
+                <br>
+                <br>
+                <button>Login</button>
+                <p>Não tem uma conta?<a href="#">Cadastre se</a></p>
+            </form>
+        </div>
+    </div>
 </body>
+
 </html>
