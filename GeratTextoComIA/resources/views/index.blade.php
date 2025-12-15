100219efd3f08db2b2a743fe234effee5dcdbdc4
Nightmeros <pereiraguilherme970@gmail.com>
2025-12-14 22:45:15 -0300

    finalização da tela teste de login

--- GeratTextoComIA/resources/views/index.blade.php
+++ GeratTextoComIA/resources/views/index.blade.php
@@ -5,23 +5,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="/css/home.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
     <div class="centralizar">
         <div id="logindivs">
-            <h1>Novo teste</h1>
-            <form action="" method="post">
-                <label for="email">Email: </label>
-                <input type="email" name="email" id="email">
-                <br><br>
-                <label for="senha">Senha: </label>
-                <input type="password" name="senha" id="senha">
-                <a href="#">Esqueci a senha</a>
-                <br>
-                <br>
-                <button>Login</button>
+            <form action="" method="post" autocomplete="off">
+                <h1>Novo teste</h1>
+                <div class="input">
+                    <label for="email"><i class="fa fa-envelope"></i> Email: </label>
+                    <input type="email1" name="email" id="email" autocomplete="off" placeholder="Email:">
+                </div>
+                <div class="input">
+                    <label for="senha"><i class="fa fa-lock"></i> Senha: </label>
+                    <input type="password1" name="senha" id="senha" autocomplete="off" placeholder="Senha">
+                </div>
+                <div class="lembrareesquecisenha">
+                    <label>
+                        <input type="checkbox">
+                        Lembrar senha
+                    </label>
+                    <a href="#">Esqueci a senha</a>
+                </div>
+ 
+                <button type="submit" class="login">Login</button>
+
                 <p>Não tem uma conta?<a href="#">Cadastre se</a></p>
             </form>
         </div>
